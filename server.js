187fc5619d9900a74136c99eec1db6b50ad96a7a
LLira34 <luislira1029@gmail.com>
2020-01-31 23:10:55 -0600

    resolviendo conflicto con el server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', function (req, res) {
+  res.sendfile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 4000);
