4bdedd84eb06547d13ee4709a2de4cd286b57556
LLira34 <luislira1029@gmail.com>
2020-01-31 22:47:13 -0600

    diseño de la pagina principal, vista usuario.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CategoriesComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
